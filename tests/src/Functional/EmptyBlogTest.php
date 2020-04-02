171912d28380e7a1140bdd7329baccbde43aba6c
git <git@3171775.no-reply.drupal.org>
2020-04-03 00:36:42 +0200

    Issue #2834732 by paddy.doyle, texas-bronius, nevergone: Incorrect no-results text when no blogs found

---
+++ tests/src/Functional/EmptyBlogTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\blog\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test for empty blogs.
+ *
+ * @group blog
+ */
+class EmptyBlogTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'blog',
+  ];
+
+  /**
+   * @var \Drupal\user\UserInterface
+   */
+  protected $blogger_no_entries;
+
+  protected function setUp() {
+    parent::setUp();
+    // create blogger user with no blog posts
+    $this->blogger_no_entries = $this->drupalCreateUser([
+      'create blog_post content',
+    ]);
+  }
+
+  /**
+   * Test empty blog lists
+   */
+  public function testEmptyLists() {
+    $this->drupalLogin($this->blogger_no_entries);
+    $this->drupalGet('blog');
+    $this->assertText('No blog entries have been created.');
+  }
+}
