7904744951571e824dc5a1700b36d7e4ebf066ec
Kurucz István <kurucz.istvan@gmail.com>
2020-06-17 02:42:24 +0200

    fix breadcrumb test with Drupal subdirectory
    see: base_path()

--- tests/src/Functional/BreadcrumbTest.php
+++ tests/src/Functional/BreadcrumbTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\blog\Functional;
 
+use Drupal\Core\Url;
 use Drupal\Tests\block\Traits\BlockCreationTrait;
 
 /**
@@ -47,10 +48,11 @@ class BreadcrumbTest extends BlogTestBase {
     $this->assertEquals(count($links), 3, 'Breadcrumb element number is correctly.');
     [$home, $blogs, $personal_blog] = $links;
     $this->assertTrue(($home->getAttribute('href') === base_path() && $home->getHtml() === 'Home'), 'Home link correctly.');
-    $this->assertTrue(($blogs->getAttribute('href') === '/blog' && $blogs->getHtml() === 'Blogs'), 'Blogs link correctly.');
+    $expected_url = Url::fromRoute('view.blog.blog_all')->toString();
+    $this->assertTrue(($blogs->getAttribute('href') === $expected_url && $blogs->getHtml() === 'Blogs'), 'Blogs link correctly.');
     $blog_name = $this->container->get('blog.lister')->userBlogTitle($blog_owner);
-    $blog_url = '/blog/' . $blog_owner->id();
-    $this->assertTrue(($personal_blog->getAttribute('href') === $blog_url && $personal_blog->getHtml() === (string) $blog_name), 'Personal blog link correctly.');
+    $expected_url = Url::fromRoute('view.blog.blog_user_all', ['arg_0' => $blog_owner->id()])->toString();
+    $this->assertTrue(($personal_blog->getAttribute('href') === $expected_url && $personal_blog->getHtml() === (string) $blog_name), 'Personal blog link correctly.');
   }
 
   /**
