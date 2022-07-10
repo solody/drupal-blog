c551dea29bda7e48691c847714bcd3f543c71eca
Kurucz István <never@nevergone.hu>
2022-07-10 21:54:55 +0200

    Issue #3283370 by nevergone: Add UninstallValidator service

--- tests/src/Functional/InstallUninstallTest.php
+++ tests/src/Functional/InstallUninstallTest.php
@@ -17,6 +17,13 @@ class InstallUninstallTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'blog',
+  ];
+
+  /**
    * The module installer service.
    *
    * @var \Drupal\Core\Extension\ModuleInstallerInterface
@@ -35,7 +42,6 @@ class InstallUninstallTest extends BrowserTestBase {
    * Tests module re-install.
    */
   public function testReinstall() {
-    $this->moduleInstaller->install(['blog']);
     $problems = $this->moduleInstaller->validateUninstall(['blog']);
     $this::assertEmpty($problems);
     $this->moduleInstaller->uninstall(['blog']);
@@ -44,5 +50,31 @@ class InstallUninstallTest extends BrowserTestBase {
     $problems = $this->moduleInstaller->validateUninstall(['blog']);
     $this::assertEmpty($problems);
     $this->moduleInstaller->uninstall(['blog']);
+  }
+
+  /**
+   * Tests module uninstall.
+   */
+  public function testUninstall() {
+    // Create blogger user.
+    $blogger = $this->drupalCreateUser([
+      'create blog_post content',
+    ]);
+    $problems = $this->moduleInstaller->validateUninstall(['blog']);
+    $this::assertEmpty($problems);
+    // Create blog posts.
+    $blog_node = $this->createNode([
+      'type' => 'blog_post',
+      'title' => $this->randomMachineName(32),
+      'uid' => $blogger->id(),
+    ]);
+    // Validate blog module uninstall.
+    $problems = $this->moduleInstaller->validateUninstall(['blog']);
+    $this::assertNotEmpty($problems);
+    // Delete blog posts.
+    $blog_node->delete();
+    // Uninstall blog module.
+    $problems = $this->moduleInstaller->validateUninstall(['blog']);
+    $this::assertEmpty($problems);
   }
 }
